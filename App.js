f1741cf33556942e32fc014c7a58c596383fdce8
nischhas <nischhal.shrestha@metropolia.fi>
2022-11-01 00:11:39 +0200

    fetched news from backend and displayed as list

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import SampleList from './component/SampleList';
+import { getAlllNews } from './services/NewsService';
 
-export default function App() {
+const App = () => {
+  const [news, setNews] = useState([]);
+  //getAlllNews(setNews);
+  console.log('App', news);
+
+  // getAlllNews function is called once after the page is rendered
+  useEffect(() => {
+    getAlllNews(setNews);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>News List!</Text>
+      <FlatList
+        data={news}
+        renderItem={({ item }) => <SampleList news={item} />}
+      />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +34,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
