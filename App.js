18fe3589242e9e97e294d3aaeee8efba1c2b9314
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-11-01 16:25:20 +0200

    Moved codes for news testing to NewsTest.js, Recoded navigation in app.js

--- App.js
+++ App.js
@@ -1,41 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import SampleList from './component/SampleList';
-import { getAlllNews } from './services/NewsService';
+import React from 'react';
 
 // Import Navigator from navigator.js
 import Navigator from './navigation/navigator';
 
 const App = () => {
-  const [news, setNews] = useState([]);
-  //getAlllNews(setNews);
-  console.log('App', news);
-
-  // getAlllNews function is called once after the page is rendered
-  useEffect(() => {
-    getAlllNews(setNews);
-  }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>News List!</Text>
-      <FlatList
-        data={news}
-        renderItem={({ item }) => <SampleList news={item} />}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <Navigator />
   );
-};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
 
 export default App;
