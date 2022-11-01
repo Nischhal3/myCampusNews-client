aa115668b61c602c48c2ff0eeb4c0bb735c2d16f
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-11-01 20:11:57 +0200

    Linear gradient and nokia icon test

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,24 +1,46 @@
-// npm install react-native-linear-gradient
+// npx expo install expo-linear-gradient
 import React, {useContext, useEffect, useState} from 'react';
-import { ImageBackground, SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet, View, Text, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const WelcomeScreen = () => {
     return (
-        <View style={styles.container}>
-            <Text>Welcome Screen</Text>
-        </View>
+        <LinearGradient 
+            start={{x: 1, y: 0}} end={{x: 0, y: 1}}
+            locations={[0, 0.24, 0.45, 1]}
+            colors={['#2A6AAA', '#004D9B', '#004D9B', '#80A5CA']} 
+            style={styles.linearBackground}
+            >
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Image source={require("../assets/nokia/nokiablue.png")} />
+                </View>
+                <View style={styles.buttonContainer}>
+
+                </View>
+            </View>
+        </LinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
+    linearBackground: {
+        flex: 1
+    },
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
     },
-    background: {
-        flex: 1
-    }
+    header: {
+        position: 'absolute',
+        top: "25%",
+        left: 0,
+        right: 0,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonContainer: {
+
+    },
 })
 
 export default WelcomeScreen;
