d9b0236ec952f7cc0df6a6fe658311910fdd8b80
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-11-02 02:50:31 +0200

    Logos Splash Screen and Fonts

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
+// npx expo install expo-font
 import React from 'react';
+import {useFonts} from 'expo-font';
 
 // Import Navigator from navigator.js
 import Navigator from './navigation/navigator';
 
 const App = () => {
+
+  let [fontsLoaded] = useFonts(
+    {"Goldman-regular": require("./assets/fonts/Goldman-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <Navigator />
