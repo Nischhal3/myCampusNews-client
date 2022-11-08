4d580c24f9906a2ba0d39ea06726e29b6330087f
Xiaoming Ma <73438688+myxmxm@users.noreply.github.com>
2022-11-08 09:42:51 +0200

    add Home screen

--- navigation/navigator.js
+++ navigation/navigator.js
@@ -1,37 +1,62 @@
 // npm install @react-navigation/native @react-navigation/native-stack
 // npm install react-native-screens react-native-safe-area-context
-import React from 'react';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import React, { useContext } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // Import Navigations
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Import Screens
-import WelcomeScreen from '../screens/WelcomeScreen';
-import LoginScreen from '../screens/LoginScreen';
-import Register from '../screens/RegisterScreen';
+import WelcomeScreen from "../screens/WelcomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import Register from "../screens/RegisterScreen";
+import Home from "../screens/Home";
+import { Context } from "../contexts/Context";
 
 const Stack = createNativeStackNavigator();
 
 const StackScreen = () => {
-    return (
-        <Stack.Navigator>
-            <>
-                <Stack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown: false}} />
-                <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
-                <Stack.Screen name='Register' component={Register} options={{headerShown: false}} />
-            </>
-        </Stack.Navigator>
-    )
-}
+  const { isLoggedIn } = useContext(Context);
+  return (
+    <Stack.Navigator>
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            options={{ headerShown: false }}
+          />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
 
 const navigator = () => {
-    return (
-        <NavigationContainer>
-                <StackScreen />
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <StackScreen />
+    </NavigationContainer>
+  );
+};
 
 export default navigator;
