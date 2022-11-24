7009f98dca937bbf8acc04da22a0f388b20d45e1
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-11-24 14:47:41 +0200

    SingleNewsUI update

--- component/ParagraphList.js
+++ component/ParagraphList.js
@@ -1,15 +1,7 @@
 import React, { useContext, useEffect, useState, useRef } from "react";
 import { Text, TouchableOpacity, View, StyleSheet, Image } from "react-native";
-import { Avatar, ListItem as RNEListItem } from "react-native-elements";
-import PropTypes from "prop-types";
-import { Context } from "../contexts/Context";
-import { getUserById } from "../services/UserService";
-import { useComment } from "../services/NewsService";
 import { baseUrl } from "../utils/variables";
 import defaultImage from "../assets/images/blank_avatar.jpg";
-import { Swipeable } from "react-native-gesture-handler";
-// import { renderRightActions } from "./SwipeableActions";
-import Animated from "react-native-reanimated";
 
 // UI Imports
 import colors from "../utils/colors";
@@ -19,13 +11,12 @@ import McIcons from "@expo/vector-icons/MaterialCommunityIcons";
 const ParagraphList = ({ paragraph }) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: `${baseUrl}/${paragraph.p_photo_name}` }} />
-      <View>
-        <Text>{paragraph.p_photo_description}</Text>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: `${baseUrl}/${paragraph.p_photo_name}` }} />
+        <Text style={styles.imageDescription}>{paragraph.p_photo_description}</Text>
       </View>
-
-      <View>
-        <Text>{paragraph.p_content}</Text>
+      <View style={styles.paragraphContainer}>
+        <Text style={styles.paragraph}>{paragraph.p_content}</Text>
       </View>
     </View>
   );
@@ -34,11 +25,31 @@ const ParagraphList = ({ paragraph }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
+  },
+  imageContainer: {
+    width: "95%",
+    alignSelf: "center",
+    marginBottom: 10,
   },
   image: {
     width: "100%",
     height: undefined,
     aspectRatio: 1.5,
+  },
+  imageDescription: {
+    padding: 2,
+    fontFamily: "IBM",
+    fontSize: fontSize.small,
+    color: colors.dark_grey,
+  },
+  paragraphContainer: {
+    // borderWidth: 1
+  },
+  paragraph: {
+    fontFamily: "IBM",
+    fontSize: fontSize.medium,
+    color: colors.dark_text,
   },
 });
 
